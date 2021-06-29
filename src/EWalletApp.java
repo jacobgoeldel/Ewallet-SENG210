8bb382ef30f1f76e0eb70ce1feb8bbf16ea124b4
Jake Goeldel <goeldelj@gmail.com>
2021-06-29 11:15:40 -0400

    Created basic setup for program

--- src/EWalletApp.java
+++ src/EWalletApp.java
@@ -1,8 +1,27 @@
 import java.util.ArrayList;
 
-public class EWalletApp {
+import javax.swing.JFrame;
+
+public class EWalletApp extends JFrame {
+	private static final long serialVersionUID = 7170541099903686382L;
+	
 	//this is the app class, has the GUI and create one object of your expense calculator class. The expense calculator class is the implementation of the Expenser interface 
 	private ArrayList<User> AllData;
+	private Expenser expenser;
+	
+	EWalletApp() {
+		expenser = new ExpenseCalculator();
+		
+		// create any UI here
+		
+		setSize(400,500);  
+		setLayout(null);  
+		setVisible(true);  
+	}
+	
 	public void CreateUser(String username, String password) {}
 
+	public static void main(String[] args) {
+		new EWalletApp();
+	}
 }
---
+++ src/ExpenseCalculator.java
@@ -0,0 +1,82 @@
+
+public class ExpenseCalculator implements Expenser {
+
+	@Override
+	public void addExpense(Expense Ex) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addMonthlyIncome(Wage W) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void PrintFullreport() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void PrintExpensereport() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void PrintIncomereport() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void PrintIncomereportbyTpe() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void PrintExpensebyType() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void exportReport(String reportTitle) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Currency convertForeignCurrency(Currency C, double amount) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean loadExpenseFile(String filePath) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean loadIncomeFile(String filePath) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int whenCanIBuy(String itemname, double price) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void updateMonthlySavings() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
