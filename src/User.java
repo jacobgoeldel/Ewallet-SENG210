68a6412836c4b11f7f4340d2b9080e704bcb381d
Zak <slocumz@ferris.edu>
2021-06-29 22:54:26 -0400

    Added wage constructor, added method within User to add income, once approved will move method attachment into ExpenserCalculator.

--- src/User.java
+++ src/User.java
@@ -12,4 +12,8 @@ public class User {
 	double monthlysavings;	
 	//should add constructor(s)
 	User(String username,String password){}
+	
+	public void AddIncome(Wage In) {
+		Income.add(In);
+	}
 }
