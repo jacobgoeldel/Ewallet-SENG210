84dacd0c2514dca1bc01840c79bfd1f2535ba758
Jake Goeldel <goeldelj@gmail.com>
2021-07-13 12:25:20 -0400

    Fixed some issues caused by resolving merge conflict

--- src/EWalletApp.java
+++ src/EWalletApp.java
@@ -15,168 +15,161 @@ import javax.swing.JTextField;
 
 public class EWalletApp extends JFrame {
 	private static final long serialVersionUID = 7170541099903686382L;
-	
-	//this is the app class, has the GUI and create one object of your expense calculator class. The expense calculator class is the implementation of the Expenser interface 
+
+	// this is the app class, has the GUI and create one object of your expense
+	// calculator class. The expense calculator class is the implementation of the
+	// Expenser interface
 	private ArrayList<User> AllData;
 	private Expenser expenser;
-	
+
 	// UI Components
 	private JButton addExpenseButton;
 	private JButton addIncomeButton;
-  private JButton itemButton;
+	private JButton itemButton;
 	private JTextArea reportOutput;
 	private JScrollPane reportScrollPane;
-	
+
 	EWalletApp() {
 		// Setup program to close after the window closes
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+		setTitle("Expense Calculator");
+
 		expenser = new ExpenseCalculator(new User("testuser", "password"));
-		
+
 		// Setup all of the layouts and window
-		
+
 		FlowLayout layout = new FlowLayout();
 		setLayout(layout);
-		
+
 		JPanel buttonpanel = new JPanel();
-		
-		GridLayout buttonLayout = new GridLayout(0,3);
+
+		GridLayout buttonLayout = new GridLayout(0, 3);
 		buttonLayout.setHgap(10);
 		buttonLayout.setVgap(10);
-		
-	    buttonpanel.setLayout(buttonLayout);
-	    
-		setSize(500,500);
+
+		buttonpanel.setLayout(buttonLayout);
+
+		setSize(600, 500);
 		setResizable(false);
-		
+
 		// create any UI here \\
-		
+
 		// add monthly expense button
 		addExpenseButton = new JButton("Add New Monthly Expense");
 		addIncomeButton = new JButton("Add New Monthly Income");
-    itemButton = new JButton("Upcoming Purchase");
-		
+		itemButton = new JButton("Upcoming Purchase");
+
 		// Create the report text area
 		reportOutput = new JTextArea("", 1, 1);
 		reportOutput.setLineWrap(true);
 		reportOutput.setEditable(false);
 		reportOutput.setVisible(true);
-	    
+
 		reportScrollPane = new JScrollPane(reportOutput);
 		reportScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 		reportScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		reportScrollPane.setPreferredSize(new Dimension(400, 400));
-		
+		reportScrollPane.setPreferredSize(new Dimension(500, 400));
+
 		// add the buttons to the top panel
-		
+
 		buttonpanel.add(addExpenseButton);
 		buttonpanel.add(addIncomeButton);
-    buttonpanel.add(itemButton);
-		
+		buttonpanel.add(itemButton);
+
 		// add button panel and report to the window
-		
+
 		add(buttonpanel);
 		add(reportScrollPane);
-		
+
 		// Setup events here \\
-		
+
 		addExpenseButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+
 				JTextField expenseName = new JTextField();
 				JTextField expenseAmount = new JTextField();
 				JTextField expenseFrequency = new JTextField();
-				
-				Object[] newExpense = {
-					    "Expense Name: ", expenseName,
-					    "Amount: ", expenseAmount,
-					    "Yearly Frequency: ", expenseFrequency
-					};
 
-				
-				int option = JOptionPane.showConfirmDialog(null, newExpense, "Add New Expense", JOptionPane.OK_CANCEL_OPTION);
-				
+				Object[] newExpense = { "Expense Name: ", expenseName, "Amount: ", expenseAmount, "Yearly Frequency: ",
+						expenseFrequency };
+
+				int option = JOptionPane.showConfirmDialog(null, newExpense, "Add New Expense",
+						JOptionPane.OK_CANCEL_OPTION);
+
 				// exit out if they canceled
-				if(option != JOptionPane.OK_OPTION)
+				if (option != JOptionPane.OK_OPTION)
 					return;
-				
-				Expense expense = new Expense(expenseName.getText(), Double.parseDouble(expenseAmount.getText()), Integer.parseInt(expenseFrequency.getText()));
+
+				Expense expense = new Expense(expenseName.getText(), Double.parseDouble(expenseAmount.getText()),
+						Integer.parseInt(expenseFrequency.getText()));
 				expenser.addExpense(expense);
-				
+
 				reportOutput.setText(expenser.PrintFullreport());
 			}
 		});
-		
+
 		addIncomeButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+
 				JTextField incomeName = new JTextField();
 				JTextField incomeAmount = new JTextField();
 				JTextField incomeMonth = new JTextField();
-				
-				Object[] newIncome = {
-					    "Income Name: ", incomeName,
-					    "Amount: ", incomeAmount,
-					    "Month: ", incomeMonth
-					};
 
-				
-				int option = JOptionPane.showConfirmDialog(null, newIncome, "Add New Income", JOptionPane.OK_CANCEL_OPTION);
-				
+				Object[] newIncome = { "Income Name: ", incomeName, "Amount: ", incomeAmount, "Month: ", incomeMonth };
+
+				int option = JOptionPane.showConfirmDialog(null, newIncome, "Add New Income",
+						JOptionPane.OK_CANCEL_OPTION);
+
 				// exit out if they canceled
-				if(option != JOptionPane.OK_OPTION)
+				if (option != JOptionPane.OK_OPTION)
 					return;
-				
-				Wage wage = new Wage(incomeName.getText(), Double.parseDouble(incomeAmount.getText()), incomeMonth.getText());
+
+				Wage wage = new Wage(incomeName.getText(), Double.parseDouble(incomeAmount.getText()),
+						incomeMonth.getText());
 				expenser.addMonthlyIncome(wage);
-				
+
 				reportOutput.setText(expenser.PrintFullreport());
 			}
 		});
-		
-		// show the window
-		setVisible(true);
-	}
-  
-  itemButton.addActionListener(new ActionListener() {
+
+		itemButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				JTextField itemName = new JTextField();
 				JTextField itemPrice = new JTextField();
-				
-				Object[] itemCheck = {
-						"Product: ", itemName,
-						"Price: ", itemPrice
-				};
-				
-				int option = JOptionPane.showConfirmDialog(null, itemCheck, "Upcoming Purchase", JOptionPane.OK_CANCEL_OPTION);
-				if(option != JOptionPane.OK_OPTION) {
+
+				Object[] itemCheck = { "Product: ", itemName, "Price: ", itemPrice };
+
+				int option = JOptionPane.showConfirmDialog(null, itemCheck, "Upcoming Purchase",
+						JOptionPane.OK_CANCEL_OPTION);
+				if (option != JOptionPane.OK_OPTION) {
 					return;
 				}
-				
+
 				int result = expenser.whenCanIBuy(itemName.getText(), Double.parseDouble(itemPrice.getText()));
-				
-				if(result == -1) {
-					JOptionPane.showMessageDialog(null, "No monthly savings founds, please update income and expenses.");
-				}
-				else if(result == 0) {
+
+				if (result == -1) {
+					JOptionPane.showMessageDialog(null,
+							"No monthly savings founds, please update income and expenses.");
+				} else if (result == 0) {
 					JOptionPane.showMessageDialog(null, "You could buy " + itemName.getText() + " right now!");
-				}
-				else if (result >= 12) {
-					JOptionPane.showMessageDialog(null, "You'll have to save for a year or more to buy " + itemName.getText() + ".");
-				}
-				else {
-					JOptionPane.showMessageDialog(null, "You'll have to save for " + result + " months before you can buy " + itemName.getText() + ".");
+				} else if (result >= 12) {
+					JOptionPane.showMessageDialog(null,
+							"You'll have to save for a year or more to buy " + itemName.getText() + ".");
+				} else {
+					JOptionPane.showMessageDialog(null, "You'll have to save for " + result
+							+ " months before you can buy " + itemName.getText() + ".");
 				}
 			}
-		}); 
-		
+		});
+
 		setVisible(true);
 	}
-	
-	public void CreateUser(String username, String password) {}
+
+	public void CreateUser(String username, String password) {
+	}
 
 	public static void main(String[] args) {
 		new EWalletApp();
